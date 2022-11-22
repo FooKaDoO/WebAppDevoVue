f82236e96cc373bbc40f9cc305e96cb66d2a73c1
martink6nnu <martin@print.ee>
2022-11-23 00:03:59 +0200

    add things

--- src/store/index.js
+++ src/store/index.js
@@ -4,14 +4,22 @@ import { createStore } from 'vuex'
 export default createStore({
   state: {
     posts: new Map([
-      [0,{PID: 0, UID: 1, message: "Noku", image: "/assets/postImages/logo.png", likes: 5, date:"2021-01-01"}],
-      [1,{PID: 1, UID: 1, message: "Noku", image: "/assets/postImages/logo.png", likes: 5, date:"2021-01-01"}],
-      [2,{PID: 2, UID: 1, message: "Noku", image: "/assets/postImages/logo.png", likes: 5, date:"2021-01-01"}]
+      [0,{PID: 0, UID: 0, message: "tere", image: "/assets/postImages/logo1.jpg", likes: 5, date:"2021-01-01"}],
+      [1,{PID: 1, UID: 1, message: "tere", image: "/assets/postImages/logo2.jpg", likes: 5, date:"2021-01-01"}],
+      [2,{PID: 2, UID: 2, message: "tere", image: "/assets/postImages/logo3.jpg", likes: 5, date:"2021-01-01"}],
+      [3,{PID: 3, UID: 1, message: "tere", image: "/assets/postImages/logo4.jpg", likes: 5, date:"2021-01-01"}],
+      [4,{PID: 4, UID: 1, message: "tere", image: "/assets/postImages/logo5.jpg", likes: 5, date:"2021-01-01"}],
+      [5,{PID: 5, UID: 2, message: "tere", image: "/assets/postImages/logo6.jpg", likes: 5, date:"2021-01-01"}],
+      [6,{PID: 6, UID: 1, message: "tere", image: "/assets/postImages/logo7.jpg", likes: 5, date:"2021-01-01"}],
+      [7,{PID: 7, UID: 0, message: "tere", image: "/assets/postImages/logo8.jpg", likes: 5, date:"2021-01-01"}],
+      [8,{PID: 8, UID: 1, message: "tere", image: "/assets/postImages/logo9.jpg", likes: 5, date:"2021-01-01"}],
+      [9,{PID: 9, UID: 1, message: "tere", image: "/assets/postImages/logo10.jpg", likes: 5, date:"2021-01-01"}],
+
     ]),
     users: new Map([
-      [0, {UID: 0, userName: "Jeekvim", pfp:"/assets/userImages/logo.png"}],
-      [1, {UID: 1, userName: "Hasbullah", pfp:"/assets/userImages/logo.png"}],
-      [2, {UID: 2, userName: "Post Malone", pfp:"/assets/userImages/logo.png"}]
+      [0, {UID: 0, userName: "Jeekvim", pfp:"/assets/userImages/pfp1.jpg"}],
+      [1, {UID: 1, userName: "Hasbullah", pfp:"/assets/userImages/pfp2.jpg"}],
+      [2, {UID: 2, userName: "Post Malone", pfp:"/assets/userImages/pfp3.jpg"}]
     ])
   },
   getters: {
