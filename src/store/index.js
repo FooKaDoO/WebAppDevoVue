92a07185aa46e92276c50f822489194a6fc827c7
FooKaDoO <mihkelhani@gmail.com>
2022-11-22 21:49:46 +0200

    postid

--- src/store/index.js
+++ src/store/index.js
@@ -3,22 +3,22 @@ import { createStore } from 'vuex'
 export default createStore({
   state: {
     posts: [
-      {PID: 0, UID: 1, message: "Noku", image: "@/assets/logo.png", likes: 5},
-      {PID: 1, UID: 1, message: "Noku", image: "@/assets/logo.png", likes: 5},
-      {PID: 2, UID: 1, message: "Noku", image: "@/assets/logo.png", likes: 5}
+      {PID: 0, UID: 1, message: "Noku", image: "/assets/postImages/logo.png", likes: 5},
+      {PID: 1, UID: 1, message: "Noku", image: "/assets/postImages/logo.png", likes: 5},
+      {PID: 2, UID: 1, message: "Noku", image: "/assets/postImages/logo.png", likes: 5}
     ],
     users: new Map([
-      [0, {UID: 0, userName: "Jeekvim", pfp:"@/assets/logo.png"}],
-      [1, {UID: 1, userName: "Hasbullah", pfp:"@/assets/logo.png"}],
-      [2, {UID: 2, userName: "Post Malone", pfp:"@/assets/logo.png"}]
+      [0, {UID: 0, userName: "Jeekvim", pfp:"/assets/userImages/logo.png"}],
+      [1, {UID: 1, userName: "Hasbullah", pfp:"/assets/userImages/logo.png"}],
+      [2, {UID: 2, userName: "Post Malone", pfp:"/assets/userImages/logo.png"}]
     ])
   },
   getters: {
     getPosts: state => {
       var getPosts = state.posts.map(post => {
           return {
-          //name: state.users.get(post.UID).userName,
-          //pfp: state.users.get(post.UID).pfp,
+          name: state.users.get(post.UID).userName,
+          pfp: state.users.get(post.UID).pfp,
           message: post.message,
           image: post.image,
           likes: post.likes
