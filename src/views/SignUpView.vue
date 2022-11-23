90d186f1540208239919b957c8aa89d5207a3933
FooKaDoO <mihkelhani@gmail.com>
2022-11-23 19:01:03 +0200

    made isValid method, normal

--- src/views/SignUpView.vue
+++ src/views/SignUpView.vue
@@ -1,19 +1,16 @@
 <template>
   <div class="signup">
-    <p v-if="errors.length">
-      <b>Please correct the following errors</b>
       <ul>
         <li v-for="e in errors" v-bind:key="e.id">
           {{e}}
         </li>
       </ul>
-    </p>
-    <form>
+    <form @submit.prevent="isValid(password)">
       <input type="text" id="email" name="email" placeholder="Your email.." v-model="name" required>
       <br>
       <input type="text" id="password" name="Password" placeholder="Your password.." v-model="password" required>
       <br>
-      <input type="submit" value="Sign up" v-on:click="isValid" >
+      <input type="submit" value="Sign up" >
     </form>
     
 
@@ -25,66 +22,30 @@ export default{
   name: "isValid",
   data(){
     return{
-      errors:[],
-      name: null,
-      password: null
+      errors:[
+        "The password's length has to be between 8 and 12 characters.",
+        "The password is required to have a number in it.",
+        "The password is required to have a special character.",
+        "The password is required to have an uppercase character.",
+        "The password is required to have the '_' symbol.",
+        "The password required to have 2 lowercase characters."
+      ]
     }
   },
   methods: {
     isValid(e){
-      console.warn("FUNCTION CALLED")
-      if(this.password==undefined){
-        console.log("Password undefined")
+      console.log(e)
+      if (
+        /^.{8,12}$/.test(e) &&
+        /\d/.test(e) &&
+        /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]/.test(e) &&
+        /[A-Z]/.test(e) &&
+        /[_]/.test(e) &&
+        /[a-z]{2,}/.test(e)
+        ) {
+        return false
       }
-      if(this.password.length>=8 || this.password.length<=15 ){
-        this.errors.push("The password's length is not between 8 and 12 characters.")
-        console.log("The password's length is not between 8 and 12 characters.")
-      } 
-      let number= false;
-      this.errors.push("The password is required to have a number in it.")
-      let specialChar= false;
-      let specList="!?,.:;-'#€%&/()={[]}§$£@©"
-      this.errors.push("The password is missing a special character.")
-      let upperCase= false
-      this.errors.push("The password is missing a uppercase character.")
-      let underScore= false
-      this.errors.push("The password is required to have the '_' symbol.")
-      let lowerCase=0
-      this.errors.push("The password required to have 2 lowercase characters.")
-      for (let i=0; i< this.password.length;i++){
-        char=this.password.charAt(i);
-        if(!isNaN(char*1)){
-          number=true;
-          this.errors.remove("The password is required to have a number in it.")
-          console.log("The password is required to have a number in it.")
-        }
-        else if(char==char.toUpperCase()){
-          upperCase=true;
-          this.errors.remove("The password is missing a uppercase character.")
-          console.log("The password is missing a uppercase character.")
-        }
-        else if(char==char.toLowerCase()){
-          lowerCase+=1;
-          if(lowerCase>=2){
-            this.errors.remove("The password required to have 2 lowercase characters.")
-            console.log("The password required to have 2 lowercase characters.")
-          }
-        }
-        else if(char=="_"){
-          underScore=true;
-          this.errors.remove("The password is required to have the '_' symbol.")
-          console.log("The password is required to have the '_' symbol.")
-        }
-        else if(specList.includes(char)){
-          specialChar=true;
-          this.errors.remove("The password is missing a special character.")
-          console.log("The password is missing a special character.")
-        }
-      }
-      if(len(errors)==0){
-        console.log("All checks passed")
-      }
-      e.preventDefault()
+      return true
     }
   }
 }
